642463f8b5b84a9ff0a6796613efc51f663519dc
Vishal Thapa <vishalthapa@Vishals-MacBook-Pro.local>
2025-09-21 19:47:55 +0545

    allowed host in project settings updated

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-&1rml9ah2kyrmimgm55-&4#+lyex492(5e254h!e^c9_o5d6+5
 DEBUG = True
 
 # ALLOWED_HOSTS = ['192.168.56.22']
-ALLOWED_HOSTS = ['192.168.56.22', '192.168.56.22:8000']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
